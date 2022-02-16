9221f054cd8f5fad26bec6405945cd19cfc4ca2e
kristy chao <kristy334420@gmail.com>
2022-02-16 17:37:40 +0800

    vegweb ver1

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+// 視窗卷軸
+$(window).scroll(function(){
+    // 判斷如果視窗高度超過 # nav ， # top-btn 加上active
+    const navtop = document.getElementById("nav")
+    // console.log($(window).scrollTop(), navtop.offsetTop);
+    if ($(window).scrollTop() > navtop.offsetTop){
+        // console.log("test")
+        $("#top-btn").addClass("active")
+    }else{
+        $("#top-btn").removeClass("active")
+    }
+
+
+})// window scroll end
